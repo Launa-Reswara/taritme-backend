4c989055110f88ddf131854c66b7870595d7b5b2
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-05-18 06:11:11 +0700

    chore: install auth-related packages

--- app.js
+++ app.js
@@ -2,20 +2,38 @@ const express = require("express");
 const compression = require("compression");
 const morgan = require("morgan");
 const logger = require("./src/utils/logger");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+dotenv.config();
 const app = express();
 
-// Start logging using morgan and winston logger
-const morganFormat =
-  ":method :url :status :res[content-length] - :response-time ms";
-app.use(morgan(morganFormat, { stream: logger.stream }));
+async function main() {
+  app.use(cors());
+  app.options("*", cors());
 
-/**
- * Optimization technique using compression
- * @see https://expressjs.com/en/advanced/best-practice-performance.html
- */
-app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Halo dari Express!");
-});
+  // Start logging using morgan and winston logger
+  const morganFormat =
+    ":method :url :status :res[content-length] - :response-time ms";
+  app.use(morgan(morganFormat, { stream: logger.stream }));
+
+  /**
+   * Optimization technique using compression
+   * @see https://expressjs.com/en/advanced/best-practice-performance.html
+   */
+  app.use(compression());
+
+  app.get("/", (req, res) => {
+    res.send("Halo dari Express!");
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server sudah jalan di port ${process.env.PORT}`);
+  });
+}
+
+main();
