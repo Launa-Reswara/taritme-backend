c10dcaa8b67611021bcb35aa4cd46c10d2a8db98
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-05-23 13:33:41 +0700

    feat(auth): login logic

--- src/handlers/authHandler.js
+++ src/handlers/authHandler.js
@@ -12,9 +12,11 @@ import { encode } from "../lib/utils/jwt.js";
  */
 export async function loginHandler(req, res) {
   try {
-    if (!req.body.email || !req.body.password || !req.body.role) {
-      res.status(401);
-      res.send({ statusCode: 401, message: "Data tidak lengkap!" });
+    if (!req.body.email || !req.body.password) {
+      res.send({
+        statusCode: 401,
+        message: "Login gagal, data tidak lengkap!",
+      });
     } else {
       const payload = {
         email: req.body.email,
@@ -24,47 +26,23 @@ export async function loginHandler(req, res) {
       // JWT token
       const newToken = encode(payload);
 
-      // WIP: insert to DB
-      /*if (res.statusCode === 200 || res.statusCode === 201) {
-        if (Object.keys(req.body).length === 2) {
-          const [results] = await connection().then((res) =>
-            res.query(
-              `SELECT * FROM users_list WHERE email = '${payload.email}' AND password = '${payload.password}'`
-            )
-          );
-
-          if (results.length) {
-            res.send({
-              status: res.statusCode,
-              message: "Login berhasil!",
-              data: results,
-              token: newToken,
-            });
-          } else {
-            res.send({
-              status: res.statusCode,
-              message:
-                "Username atau password yang dimasukkan salah! Silahkan coba lagi!",
-              data: null,
-              token: null,
-            });
-          }
-        }
+      if (payload.email === "halo@ekel.dev" && payload.password === "ekel123") {
+        res.status(200);
+        res.send({
+          statusCode: res.statusCode,
+          message: "Login berhasil!",
+          data: payload,
+          token: newToken,
+        });
       } else {
         res.send({
-          status: res.statusCode,
-          message: res.statusMessage,
-          data: null,
-          token: null,
+          statusCode: 401,
+          message: "Login gagal, data yang dimasukkan salah!",
         });
-      }*/ res.send({
-        status: res.statusCode,
-        message: "Login berhasil!",
-        data: payload,
-        token: newToken,
-      });
+      }
     }
   } catch (err) {
+    res.status(500);
     res.send({
       status: 500,
       message: err.message,
@@ -90,53 +68,9 @@ export async function registrationHandler(req, res) {
       name: req.name,
       email: req.email,
       password: req.password,
-      role: req.role,
     };
 
-    const newToken = encode(payload);
-
-    if (!payload.name || !payload.email || !payload.password || !payload.role) {
-      // WIP: insert to DB
-      /*if (res.statusCode === 200 || res.statusCode === 201) {
-        if (Object.keys(req.body).length === 2) {
-          const availableUser = await checkUser(payload);
-
-        if (availableUser) {
-          res.status(403);
-          res.send({
-            status: 403,
-            message: "Data ini sudah pernah didaftarkan akun!",
-          });
-        } else {
-          await connection().then((res) =>
-            res.execute(
-              `INSERT INTO users(email, password, role) VALUES('${
-                payload.email
-              }', '${encode(payload.password)}', '${
-                payload.email === process.env.ADMIN_EMAIL &&
-                payload.password === process.env.ADMIN_PASSWORD
-                  ? "admin"
-                  : "guest"
-              }')`
-            )
-          );
-
-          res.send({
-            status: res.statusCode,
-            message: "Pembuatan akun berhasil!",
-          });
-        }
-      }
-    } else {
-      res.status(res.statusCode);
-      res.send({
-        status: res.statusCode,
-        message: res.statusMessage,
-        data: null,
-        token: null,
-      });
-    }*/
-
+    if (!payload.name || !payload.email || !payload.password) {
       res.status(200);
       res.send({
         statusCode: 200,
@@ -144,6 +78,15 @@ export async function registrationHandler(req, res) {
       });
     }
   } catch (err) {
+    res.status(500);
+    res.send({ statusCode: 500, message: err.message });
+  }
+}
+
+export async function adminHandler() {
+  try {
+  } catch (err) {
+    res.status(500);
     res.send({ statusCode: 500, message: err.message });
   }
 }
