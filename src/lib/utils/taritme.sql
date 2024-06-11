0e51a68a2ac18835db8ecd257f808bdf889bd953
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-06-11 11:39:18 +0700

    chore: sql

--- src/lib/utils/taritme.sql
+++ src/lib/utils/taritme.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 02, 2024 at 08:14 AM
+-- Generation Time: Jun 11, 2024 at 04:26 AM
 -- Server version: 8.0.37
 -- PHP Version: 8.2.19
 
@@ -22,45 +22,6 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `taritme` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `taritme`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `arsip_kesenian`
---
-
-DROP TABLE IF EXISTS `arsip_kesenian`;
-CREATE TABLE `arsip_kesenian` (
-  `id` int NOT NULL,
-  `author` varchar(30) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `date` date DEFAULT NULL,
-  `preview_writing` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `preview_image` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `total_like` int NOT NULL,
-  `total_comments` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `arsip_kesenian`
---
-
-INSERT INTO `arsip_kesenian` (`id`, `author`, `title`, `date`, `preview_writing`, `preview_image`, `total_like`, `total_comments`) VALUES
-(1, 'Leonardo Da Vince', 'Tari Piring', '2024-05-31', 'Tari Piriang atau Tari Piring merupakan salah satu tarian tradisional di Indonesia yang berasal dari Suku Minangkabau. Secara tradisional, Tari Piring berasal dari Kota Solok, Provinsi Sumatera barat. Saat ini', 'https://res.cloudinary.com/da0dcs88v/image/upload/f_auto,q_auto/v1/taritme/arsip_kesenian/yujkyi5kcrw4slv9uws3', 999, 12),
-(2, 'Leonardo da Vince', 'Tari Indang Badindin', '2024-05-31', 'Tari Dindin Badindin ini merupakan salah satu tari tradisional yang berasal dari Pariaman, Provinsi Sumatera Barat. Indang sendiri berarti gendang kecil, tarian ini mirip dengan Tari Saman yang', 'https://res.cloudinary.com/da0dcs88v/image/upload/f_auto,q_auto/v1/taritme/arsip_kesenian/lnpm7lad27lkjthpzxxb', 999, 12);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `detail_arsip_kesenian`
---
-
-DROP TABLE IF EXISTS `detail_arsip_kesenian`;
-CREATE TABLE `detail_arsip_kesenian` (
-  `id` int NOT NULL,
-  `arsip_kesenian_id` int NOT NULL,
-  `content` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -126,7 +87,8 @@ CREATE TABLE `komunitas_link_socialmedia` (
   `facebook` varchar(100) DEFAULT NULL,
   `instagram` varchar(100) DEFAULT NULL,
   `twitter` varchar(100) DEFAULT NULL,
-  `whatsapp` varchar(100) DEFAULT NULL
+  `whatsapp` varchar(100) DEFAULT NULL,
+  `komunitas_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -155,13 +117,37 @@ CREATE TABLE `pelatih_tari` (
 --
 
 INSERT INTO `pelatih_tari` (`id`, `email`, `name`, `no_hp`, `description`, `image`, `rating`, `price`, `created_at`, `total_review`, `status`) VALUES
-(1, 'luna@lunamay.com', 'Luna Gaya', '081221941140214', 'dskngmkdsfgmkdmkfg', 'https://res.cloudinary.com/da0dcs88v/image/upload/v1717304551/taritme/pelatih_tari/tp9cwqpi2p1mdkate1xd.png', 5, 303000, '2024-05-27 15:40:14', 10, 'Aktif'),
-(2, '', 'Retno Maruti', '0', 'Instruktur tari Sumatra Barat yang memberikan ilmu nya melalui kursus tari.', 'https://res.cloudinary.com/da0dcs88v/image/upload/f_auto,q_auto/v1/taritme/pelatih_tari/dn4ccifygbvi6q9udjae', 5, 100000, '2024-05-27 20:55:07', 10, 'Aktif'),
-(3, '', 'Ayu Bulantrisna', '0', 'Instruktur tari Sumatra Barat yang memberikan ilmu nya melalui kursus tari.', 'https://res.cloudinary.com/da0dcs88v/image/upload/f_auto,q_auto/v1/taritme/pelatih_tari/hndjpv2fl9wwbpch284m', 5, 100000, '2024-05-27 20:55:35', 10, 'Aktif'),
-(4, '', 'Sandrina', '0', 'Instruktur tari Sumatra Barat yang memberikan ilmu nya melalui kursus tari.', 'https://res.cloudinary.com/da0dcs88v/image/upload/f_auto,q_auto/v1/taritme/pelatih_tari/g3qtmk6fs4wkovuffxya', 5, 100000, '2024-05-27 20:55:46', 10, 'Aktif'),
-(5, '', 'Soimah', '0', 'Instruktur tari Sumatra Barat yang memberikan ilmu nya melalui kursus tari.', 'https://res.cloudinary.com/da0dcs88v/image/upload/f_auto,q_auto/v1/taritme/pelatih_tari/rfqjasbi0x6m0q0fw295', 5, 100000, '2024-05-27 20:56:00', 10, 'Aktif'),
-(6, '', 'Lena Guslina', '0', 'Instruktur tari Sumatra Barat yang memberikan ilmu nya melalui kursus tari.', 'https://res.cloudinary.com/da0dcs88v/image/upload/f_auto,q_auto/v1/taritme/pelatih_tari/xjuriqyfarzcmulp1hcj', 5, 100000, '2024-05-27 20:56:11', 10, 'Aktif'),
-(7, '', 'Agnez Mo', '0', 'Instruktur tari Sumatra Barat yang memberikan ilmu nya melalui kursus tari.', 'https://res.cloudinary.com/da0dcs88v/image/upload/f_auto,q_auto/v1/taritme/pelatih_tari/itbj2vsi65nhxmwhzqfg', 5, 100000, '2024-05-27 20:56:21', 10, 'Aktif');
+(1, 'luna@lunamaya.com', 'Luna Maya', '081234567890', 'Seorang pelatih tari berpengalaman yang telah mengajar seni tari selama lebih dari lima tahun, memiliki keahlian dalam menari berbagai jenis tarian, termasuk tarian daerah Sumatera Barat.', 'https://res.cloudinary.com/da0dcs88v/image/upload/v1717355540/taritme/pelatih_tari/vs1wjlv7raij2kq4zr7f.png', 5, 100000, '2024-05-27 15:40:14', 10, 'Aktif'),
+(3, 'ayubulan@trisna.com', 'Ayu Bulantrisna', '084561237893', 'Seorang pelatih tari berpengalaman yang telah mengajar seni tari selama lebih dari lima tahun, memiliki keahlian dalam menari berbagai jenis tarian, termasuk tarian daerah Sumatera Barat.', 'https://res.cloudinary.com/da0dcs88v/image/upload/v1717386240/taritme/pelatih_tari/zoxxmklixm1kozhtwguf.png', 5, 100000, '2024-05-27 20:55:35', 10, 'Aktif'),
+(4, 'sandrina@sandrina.com', 'Sandrina', '082347861029', 'Instruktur tari Sumatra Barat yang memberikan ilmu nya melalui kursus tari.', 'https://res.cloudinary.com/da0dcs88v/image/upload/v1717386835/taritme/pelatih_tari/jpu1qtureum3jleq33ff.png', 5, 100000, '2024-05-27 20:55:46', 10, 'Aktif'),
+(5, 'soim@soimah.com', 'Soimah', '089012465234', 'Seorang pelatih tari berpengalaman yang telah mengajar seni tari selama lebih dari lima tahun, memiliki keahlian dalam menari berbagai jenis tarian, termasuk tarian daerah Sumatera Barat.', 'https://res.cloudinary.com/da0dcs88v/image/upload/v1717386921/taritme/pelatih_tari/orht1d2an5g6th0htio7.png', 5, 100000, '2024-05-27 20:56:00', 10, 'Aktif'),
+(6, 'lena@guslina.com', 'Lena Guslina', '080987654321', 'Seorang pelatih tari berpengalaman yang telah mengajar seni tari selama lebih dari lima tahun, memiliki keahlian dalam menari berbagai jenis tarian, termasuk tarian daerah Sumatera Barat.', 'https://res.cloudinary.com/da0dcs88v/image/upload/v1717385114/taritme/pelatih_tari/utg8v6veyzqpjmdb73bm.png', 5, 100000, '2024-06-02 20:25:08', 10, 'Aktif'),
+(7, 'agnezmo@agnez.com', 'Agnez Mo', '089871234561', 'Seorang pelatih tari berpengalaman yang telah mengajar seni tari selama lebih dari lima tahun, memiliki keahlian dalam menari berbagai jenis tarian, termasuk tarian daerah Sumatera Barat.', 'https://res.cloudinary.com/da0dcs88v/image/upload/v1717386033/taritme/pelatih_tari/ihrim21n2wzctgx8uuly.png', 5, 100000, '2024-06-02 20:40:27', 10, 'Aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penilaian_pelatih_tari`
+--
+
+DROP TABLE IF EXISTS `penilaian_pelatih_tari`;
+CREATE TABLE `penilaian_pelatih_tari` (
+  `id` int NOT NULL,
+  `pelatih_tari_id` int NOT NULL,
+  `pelatih_tari_name` varchar(100) NOT NULL,
+  `rating` int NOT NULL,
+  `comment` text NOT NULL,
+  `users_email` varchar(100) NOT NULL,
+  `users_name` varchar(100) NOT NULL,
+  `order_id` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `penilaian_pelatih_tari`
+--
+
+INSERT INTO `penilaian_pelatih_tari` (`id`, `pelatih_tari_id`, `pelatih_tari_name`, `rating`, `comment`, `users_email`, `users_name`, `order_id`) VALUES
+(1, 1, 'Luna Maya', 5, 'Bagus pengajarannya kak Luna Maya', 'yui@yui.app', 'Katou Megumi', 'TRM-ITEM-85XECWnK_f');
 
 -- --------------------------------------------------------
 
@@ -173,13 +159,13 @@ DROP TABLE IF EXISTS `riwayat_kursus`;
 CREATE TABLE `riwayat_kursus` (
   `id` int NOT NULL,
   `email` varchar(50) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `image` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `price` int NOT NULL,
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `rating` decimal(10,0) NOT NULL,
-  `total_review` int NOT NULL,
-  `users_id` int NOT NULL
+  `pelatih_tari_name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `pelatih_tari_image` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `pelatih_tari_price` int NOT NULL,
+  `pelatih_tari_description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `users_id` int NOT NULL,
+  `pelatih_tari_id` int NOT NULL,
+  `order_id` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -201,48 +187,37 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(3, 'Yui Hirasawa', 'yui@yui.app', '$2b$10$iK7j67vy8etIxty.ieQ4me0c9Brvu1V17yyMh2fyam0DlMRwHQIpa'),
-(5, 'Haikel Ilham Hakim', 'halo@ekel.dev', '$2b$10$pPqLODqGPa6ZwOXWuHA8tOmiBachOmjatCfRllY9aEjEccRR.uin.');
+(1, 'Katou Megumi', 'yui@yui.app', '$2b$10$5QfQMtJ22.eS5XJidCeCtOqF3uG8nY1WJu6kgs6ERMH.4.mTlQ9YO'),
+(4, 'Yui Hirasawa', 'hirasawa@sawa.dev', '$2b$10$Iq6OFJplTMxB6UxPDE5TTOVL5/x8FsMUeaWZGtZ7MgUb3O3cIzj4u');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users_payment_pelatih_tari`
+-- Table structure for table `users_profile`
 --
 
-DROP TABLE IF EXISTS `users_payment_pelatih_tari`;
-CREATE TABLE `users_payment_pelatih_tari` (
+DROP TABLE IF EXISTS `users_profile`;
+CREATE TABLE `users_profile` (
   `id` int NOT NULL,
-  `user_email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `users_id` int NOT NULL,
-  `is_users_already_paid` tinyint(1) NOT NULL,
-  `pelatih_tari_id` int NOT NULL,
-  `pelatih_tari_name` varchar(50) NOT NULL
+  `jenis_kelamin` enum('Laki-laki','Perempuan') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `bio` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `no_hp` varchar(20) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `image` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'https://via.placeholder.com/150'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `users_payment_pelatih_tari`
+-- Dumping data for table `users_profile`
 --
 
-INSERT INTO `users_payment_pelatih_tari` (`id`, `user_email`, `users_id`, `is_users_already_paid`, `pelatih_tari_id`, `pelatih_tari_name`) VALUES
-(2, 'yui@yui.app', 3, 0, 1, 'Luna Maya');
+INSERT INTO `users_profile` (`id`, `users_id`, `jenis_kelamin`, `bio`, `no_hp`, `age`, `image`) VALUES
+(1, 1, 'Perempuan', 'sfdfjk', '081234567890', 20, 'https://res.cloudinary.com/da0dcs88v/image/upload/v1717857521/taritme/users_profile/qur0plr7vkq1z4vk9sh0.jpg'),
+(2, 4, NULL, NULL, NULL, NULL, 'https://via.placeholder.com/150');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `arsip_kesenian`
---
-ALTER TABLE `arsip_kesenian`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `detail_arsip_kesenian`
---
-ALTER TABLE `detail_arsip_kesenian`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `arsip_kesenian_id` (`arsip_kesenian_id`);
 
 --
 -- Indexes for table `detail_pelatih_tari`
@@ -261,7 +236,8 @@ ALTER TABLE `komunitas`
 -- Indexes for table `komunitas_link_socialmedia`
 --
 ALTER TABLE `komunitas_link_socialmedia`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_komunitas_id` (`komunitas_id`);
 
 --
 -- Indexes for table `pelatih_tari`
@@ -270,11 +246,19 @@ ALTER TABLE `pelatih_tari`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `penilaian_pelatih_tari`
+--
+ALTER TABLE `penilaian_pelatih_tari`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pelatih_tari_id` (`pelatih_tari_id`);
+
+--
 -- Indexes for table `riwayat_kursus`
 --
 ALTER TABLE `riwayat_kursus`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_users_id` (`users_id`);
+  ADD KEY `fk_users_id` (`users_id`),
+  ADD KEY `pelatih_tari_id_riwayat_kursus_ibfk_1` (`pelatih_tari_id`);
 
 --
 -- Indexes for table `users`
@@ -283,22 +267,15 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users_payment_pelatih_tari`
+-- Indexes for table `users_profile`
 --
-ALTER TABLE `users_payment_pelatih_tari`
+ALTER TABLE `users_profile`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `users_id` (`users_id`),
-  ADD KEY `users_payment_pelatih_tari_ibfk_2` (`pelatih_tari_id`);
+  ADD KEY `users_id` (`users_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `arsip_kesenian`
---
-ALTER TABLE `arsip_kesenian`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `detail_pelatih_tari`
@@ -313,32 +290,44 @@ ALTER TABLE `komunitas`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `komunitas_link_socialmedia`
+--
+ALTER TABLE `komunitas_link_socialmedia`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `pelatih_tari`
 --
 ALTER TABLE `pelatih_tari`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `penilaian_pelatih_tari`
+--
+ALTER TABLE `penilaian_pelatih_tari`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `riwayat_kursus`
+--
+ALTER TABLE `riwayat_kursus`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `users_payment_pelatih_tari`
+-- AUTO_INCREMENT for table `users_profile`
 --
-ALTER TABLE `users_payment_pelatih_tari`
+ALTER TABLE `users_profile`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `detail_arsip_kesenian`
---
-ALTER TABLE `detail_arsip_kesenian`
-  ADD CONSTRAINT `detail_arsip_kesenian_ibfk_1` FOREIGN KEY (`arsip_kesenian_id`) REFERENCES `arsip_kesenian` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `detail_pelatih_tari`
@@ -347,17 +336,29 @@ ALTER TABLE `detail_pelatih_tari`
   ADD CONSTRAINT `fk_pelatih_tari_id` FOREIGN KEY (`pelatih_tari_id`) REFERENCES `pelatih_tari` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `komunitas_link_socialmedia`
+--
+ALTER TABLE `komunitas_link_socialmedia`
+  ADD CONSTRAINT `fk_komunitas_id` FOREIGN KEY (`komunitas_id`) REFERENCES `komunitas` (`id`);
+
+--
+-- Constraints for table `penilaian_pelatih_tari`
+--
+ALTER TABLE `penilaian_pelatih_tari`
+  ADD CONSTRAINT `penilaian_pelatih_tari_ibfk_1` FOREIGN KEY (`pelatih_tari_id`) REFERENCES `pelatih_tari` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `riwayat_kursus`
 --
 ALTER TABLE `riwayat_kursus`
-  ADD CONSTRAINT `fk_users_id` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `fk_users_id` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `pelatih_tari_id_riwayat_kursus_ibfk_1` FOREIGN KEY (`pelatih_tari_id`) REFERENCES `pelatih_tari` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `users_payment_pelatih_tari`
+-- Constraints for table `users_profile`
 --
-ALTER TABLE `users_payment_pelatih_tari`
-  ADD CONSTRAINT `users_payment_pelatih_tari_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `users_payment_pelatih_tari_ibfk_2` FOREIGN KEY (`pelatih_tari_id`) REFERENCES `pelatih_tari` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `users_profile`
+  ADD CONSTRAINT `users_profile_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
